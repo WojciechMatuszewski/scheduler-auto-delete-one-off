5ee3b49a418538a9246c9d903c53c857eefc3110
Wojciech Matuszewski <wm.matuszewski@gmail.com>
2023-04-08 06:57:12 +0200

    update

--- README.md
+++ README.md
@@ -6,4 +6,34 @@
 
 ## The architecture
 
-TODO
+WIP
+
+## Learnings
+
+- If you do not specify the _schedule group_, the group name will be "default".
+
+  - TODO: Check how the deletion of the whole group works.
+
+- The scheduler allows you to create schedules for dates that are in the past.
+
+  - They probably forgot about the validation here, but is it even worth validating?
+
+- **Once again, I forgot how the `iam:PassRole` works**. I should be re-hashing those mechanisms more often.
+
+  - The `PassRole` **denotes that role A can pass role B onto a given service**.
+
+    - The most common example is with EC2, where a user has to have `iam:PassRole` permission defined to attach a given role to the EC2 instance. Otherwise, some users with limited permissions could attach the Admin role to EC2 instance. That would be very dangerous, especially when that user could log in to that instance.
+
+    - In **the context of EventBridge Scheduler and SFN**, the **SFN role has to have permissions to `PassRole` the Scheduler needs to perform an action when the schedule is executed**.
+
+    - To my surprise, nobody seem to be talking about the **additional IAM conditions that one should add to the `PassRole` action**.
+
+      - The `iam:PassedToService` is very handy here.
+
+    - The **`iam:PassRole` action does not appear to be logged in CloudTrail**. This makes it very hard to debug.
+
+        > PassRole is not an API call. PassRole is a permission, meaning no CloudTrail logs are generated for IAM PassRole. [Source](https://docs.aws.amazon.com/IAM/latest/UserGuide/id_roles_use_passrole.html).
+
+      - In addition to the `iam:PassedToService` condition key, I've also tried to implement the `iam:AssociatedResourceARN` condition. I was unable to do so. I think the Scheduler does not support this condition key for the `iam:PassRole`.
+
+        - Looking at the CloudTrail trace when the service performs the `AssumeRole` call, there is no information about the underlying resource it assumes role for. Only the ARN of the role is visible.
--- lib/eb-scheduler-auto-delete-stack.ts
+++ lib/eb-scheduler-auto-delete-stack.ts
@@ -4,5 +4,154 @@ import { Construct } from "constructs";
 export class EbSchedulerAutoDeleteStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const machineRole = new cdk.aws_iam.Role(this, "MachineRole", {
+      assumedBy: new cdk.aws_iam.ServicePrincipal("states.amazonaws.com")
+    });
+
+    const scheduleTaskTarget = new cdk.aws_lambda.Function(
+      this,
+      "ScheduleTaskTarget",
+      {
+        code: new cdk.aws_lambda.InlineCode(`
+          module.exports.handler = async (event) => {
+            console.log("works!");
+          }
+        `),
+        runtime: cdk.aws_lambda.Runtime.NODEJS_18_X,
+        handler: "index.handler"
+      }
+    );
+
+    const invokeScheduleTargetRole = new cdk.aws_iam.Role(
+      this,
+      "InvokeScheduleTargetRole",
+      {
+        assumedBy: new cdk.aws_iam.ServicePrincipal("scheduler.amazonaws.com"),
+        inlinePolicies: {
+          allowInvokeTarget: new cdk.aws_iam.PolicyDocument({
+            statements: [
+              new cdk.aws_iam.PolicyStatement({
+                actions: ["lambda:InvokeFunction"],
+                resources: [scheduleTaskTarget.functionArn]
+              })
+            ]
+          })
+        }
+      }
+    );
+
+    const schedulerArn = cdk.Arn.format(
+      {
+        resource: "schedule/default/*",
+        service: "scheduler"
+      },
+      this
+    );
+    /**
+     * Allow the scheduler to use the `invokeScheduleTargetRole` to invoke the `scheduleTaskTarget`.
+     * If we did not have the iam:PassRole, an attacker might attach an AdminPolicy with a random SFN.
+     */
+    machineRole.addToPolicy(
+      new cdk.aws_iam.PolicyStatement({
+        actions: ["iam:PassRole"],
+        resources: [invokeScheduleTargetRole.roleArn],
+        conditions: {
+          StringEquals: {
+            "iam:PassedToService": "scheduler.amazonaws.com"
+          }
+          /**
+           * It appears not to be supported by the Scheduler.
+           */
+          // ArnLike: {
+          //   // "iam:AssociatedResourceARN": [scheduleTaskTarget.functionArn]
+          // }
+        }
+      })
+    );
+
+    const scheduleName = cdk.aws_stepfunctions.JsonPath.format(
+      "schedule_{}",
+      cdk.aws_stepfunctions.JsonPath.stringAt("$$.Execution.Name")
+    );
+
+    const createScheduleTask = new cdk.aws_stepfunctions_tasks.CallAwsService(
+      this,
+      "CreateSchedule",
+      {
+        service: "scheduler",
+        action: "createSchedule",
+        iamResources: [schedulerArn],
+        parameters: {
+          FlexibleTimeWindow: {
+            Mode: "OFF"
+          },
+          Name: scheduleName,
+          ScheduleExpression: cdk.aws_stepfunctions.JsonPath.format(
+            "at({})",
+            cdk.aws_stepfunctions.JsonPath.stringAt("$.timestamp")
+          ),
+          ScheduleExpressionTimezone: "UTC",
+          Target: {
+            Arn: scheduleTaskTarget.functionArn,
+            RoleArn: invokeScheduleTargetRole.roleArn,
+            Input: {
+              scheduleName
+            }
+          }
+        },
+        resultPath: cdk.aws_stepfunctions.JsonPath.DISCARD
+      }
+    );
+
+    const transformSchedulerTimestamp = new cdk.aws_stepfunctions.Pass(
+      this,
+      "TransformTimestamp",
+      {
+        parameters: {
+          transformedTimestamp: cdk.aws_stepfunctions.JsonPath.format(
+            "{}{}",
+            cdk.aws_stepfunctions.JsonPath.stringAt("$.timestamp"),
+            "Z"
+          )
+        }
+      }
+    );
+
+    const waitForTimestamp = new cdk.aws_stepfunctions.Wait(
+      this,
+      "WaitForTimestamp",
+      {
+        time: cdk.aws_stepfunctions.WaitTime.timestampPath(
+          "$.transformedTimestamp"
+        )
+      }
+    );
+
+    const deleteScheduleTask = new cdk.aws_stepfunctions_tasks.CallAwsService(
+      this,
+      "DeleteSchedule",
+      {
+        service: "scheduler",
+        action: "deleteSchedule",
+        iamResources: [schedulerArn],
+        parameters: {
+          Name: scheduleName
+        }
+      }
+    );
+
+    const machine = new cdk.aws_stepfunctions.StateMachine(this, "Machine", {
+      definition: createScheduleTask
+        /**
+         * Race condition.
+         * Since the deletion happens at the same time the schedule is executed, the schedule might never get executed.
+         * Can we manipulate the date somehow? I doubt it.
+         */
+        .next(transformSchedulerTimestamp)
+        .next(waitForTimestamp)
+        .next(deleteScheduleTask),
+      role: machineRole
+    });
   }
 }
